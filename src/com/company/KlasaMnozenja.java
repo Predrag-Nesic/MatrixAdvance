768729ebf841fee2ecb969487260eec278af0701
Predrag Nesic <54184774+Predrag-Nesic@users.noreply.github.com>
2023-03-11 13:20:37 +0100

    Update KlasaMnozenja.java

--- src/com/company/KlasaMnozenja.java
+++ src/com/company/KlasaMnozenja.java
@@ -2,27 +2,24 @@ package com.company;
 
 public class KlasaMnozenja extends Thread {
     private Matrica a, b;
-    private int vrsta;
-    private int[] rezultat;
+    private int vrsta, kolona;
+    private int rezultat;
 
-    public KlasaMnozenja(Matrica a, Matrica b, int i) {
+    public KlasaMnozenja(Matrica a, Matrica b, int vrsta, int kolona) {
         this.a = a;
         this.b = b;
-        vrsta = i;
-        rezultat = new int[b.dohvatiVrstu(i).length];
+        this.vrsta = vrsta;
+        this.kolona = kolona;
         start();
     }
 
-    @Override
     public void run() {
         for(int i = 0; i < a.dohvatiVrstu(vrsta).length; i++) {
-            for(int j = 0; j < b.dohvatiKolonu(vrsta).length; j++) {
-                rezultat[i] += a.dohvatiVrstu(vrsta)[j] * b.dohvatiKolonu(i)[j];
-            }
+            rezultat += a.dohvatiVrstu(vrsta)[i] * b.dohvatiKolonu(kolona)[i];
         }
     }
 
-    public int[] getRezultat() {
+    public int getRezultat() {
         return rezultat;
     }
 }
